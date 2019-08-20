261943e697f2e4c2faf2867b3ec5209bc0d5b78f
wisew <ww_ttxy@163.com>
2019-08-20 09:37:18 +0800

    添加增加spark stteaming简单demo

---
+++ src/main/scala/com/wangwei/spark/streaming/NetworkWC.scala
@@ -0,0 +1,29 @@
+package com.wangwei.spark.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * streaming hello world入门
+  */
+object NetworkWC {
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: NetworkWordCount <hostname> <port>")
+      System.exit(1)
+    }
+
+
+    // Create the context with a 1 second batch size
+    val sparkConf = new SparkConf().setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_AND_DISK_SER)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+    ssc.stop()
+  }
+}
